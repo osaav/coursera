666b636cc0aecd398da425d6b91b70ed5fd69779
alexander.osipov <alexander.osipov@streamlabs.ru>
2019-05-31 19:24:55 +0300

    task_2_15 done

---
+++ Coursera/task_2_15.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <set>
+#include <string>
+
+using namespace std;
+
+int main() {
+	int n;
+	cin >> n;
+	set<string> words;
+	for (int i = 0; i < n; ++i) {
+		string word;
+		cin >> word;
+		words.insert(word);
+	}
+	cout << words.size() << endl;
+	cin >> n;
+	return 0;
+}
